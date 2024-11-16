9cd8315e074b2a68aa1b449cd7d0a37fc6fb1d1d
zjx <1240348479@qq.com>
2024-11-16 18:21:06 +0800

    reconstruct configs, remove the param split_file in CSLDailyKeypointDataset.py and CSLDailyKeypointDataModule.py

--- slr/datasets/KeypointDatasets/CSLDailyKeypointDataModule.py
+++ slr/datasets/KeypointDatasets/CSLDailyKeypointDataModule.py
@@ -13,7 +13,6 @@ class CSLDailyKeypointDataModule(KeypointBaseDataModule):
     def __init__(
             self,
             keypoints_file: str = None,
-            split_file: str = None,
             batch_size: int = 2,
             num_workers: int = 8,
             transform: [callable, dict] = None,
@@ -25,7 +24,6 @@ class CSLDailyKeypointDataModule(KeypointBaseDataModule):
 
         # Ensure all directory paths are set correctly
         self.keypoints_file = os.path.abspath(keypoints_file) if keypoints_file else None
-        self.split_file = os.path.abspath(split_file) if split_file else None
 
     @override
     def load_dataset(self, mode):
@@ -42,7 +40,6 @@ class CSLDailyKeypointDataModule(KeypointBaseDataModule):
         tokenizer = self.tokenizers.get(mode, None)
         return CSLDailyKeypointDataset(
             keypoints_file=self.keypoints_file,
-            split_file=self.split_file,
             mode=mode,
             transform=transform,
             tokenizer=tokenizer
--- slr/main.py
+++ slr/main.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 import hydra
 import lightning as L
+import psutil
 import torch
 import transformers
 import wandb
@@ -25,31 +26,48 @@ def main(cfg: DictConfig):
     Initializes the training environment, sets up logging, checkpoints, and
     data modules, then trains and tests the model based on the provided config.
     """
+    #######################################################################################
     # Set num threads
-    torch.set_num_threads(cfg.get('torch_num_threads', 2))
+    torch_num_threads = cfg.get('torch_num_threads', psutil.cpu_count(logical=False))
+    torch.set_num_threads(torch_num_threads)
     # Set precision for float32 matmul operations
-    torch.set_float32_matmul_precision(cfg.get('torch_float32_matmul_precision', 'high'))
-    seed = set_seed(cfg.get('seed', -1), workers=True)
+    torch_float32_matmul_precision = cfg.get('torch_float32_matmul_precision', 'high')
+    torch.set_float32_matmul_precision(torch_float32_matmul_precision)
+    # Set seed
+    seed = cfg.get('seed', -1)
+    set_seed(seed, workers=True)
 
+    #######################################################################################
     # Define project, name, and times
     project = cfg.get('project', 'default_project')
     name = cfg.get('name', 'default_name')
     times = cfg.get('times', 0)
-
+    # Common parameters
+    dataset_name = cfg.dataset_name
+    dataset_type = cfg.dataset_type
+    model_name = cfg.model_name
     # Create save directory
     save_dir = os.path.join(os.path.abspath(cfg.get('save_dir', '../experiments')), project, name, str(times))
     os.makedirs(save_dir, exist_ok=True)
+    #
+    ckpt_file = cfg.get('checkpoint', None)
+    # Is convert_to_onnx enabled?
+    is_save_onnx = cfg.get('convert_to_onnx', False)
 
-    # Common parameters
-    dataset_name = cfg.dataset_name
-    model_name = cfg.model_name
+    #######################################################################################
+    data_cfgs = cfg.dataset.get('data_cfgs', None)
+    kps_file = cfg.dataset.get('keypoints_file', None)
+    gloss_gt_file = cfg.dataset.get('gloss_groundtruth_file', None)
+    gloss_vocab_file = cfg.dataset.get('gloss_vocab_file', None)
 
+    #######################################################################################
     # Initialize tokenizer, decoder, and evaluator
-    tokenizer = SimpleTokenizer(**cfg.tokenizer)
+    tokenizer = SimpleTokenizer(vocab_file=gloss_vocab_file, **cfg.tokenizer)
     # tokenizer = transformers.BertTokenizer.from_pretrained('/new_home/xzj23/workspace/SLR/.cache/huggingface/bert-base-german-dbmdz-uncased')
     ctc_decoder = CTCBeamSearchDecoder(tokenizer=tokenizer, **cfg.decoder)
-    evaluator = Evaluator(dataset=dataset_name, **cfg.evaluator)
+    evaluator = Evaluator(gt_file=gloss_gt_file, dataset=dataset_name, **cfg.evaluator)
 
+    #######################################################################################
     # Initialize WandbLogger
     wandb.require("core")
     wandb_logger = WandbLogger(
@@ -59,13 +77,26 @@ def main(cfg: DictConfig):
         **cfg.logger
     )
 
+    #######################################################################################
     # Initialize data module
-    data_module = DataModuleClassDict[dataset_name](
-        transform=transform,
-        tokenizer=tokenizer,
-        **cfg.dataset
-    )
+    if dataset_type == 'video':
+        data_module = DataModuleClassDict[dataset_name](
+            transform=transform,
+            tokenizer=tokenizer,
+            **data_cfgs,
+            **cfg.dataloader
+        )
+    elif dataset_type == 'keypoint':
+        data_module = DataModuleClassDict[dataset_name](
+            transform=transform,
+            tokenizer=tokenizer,
+            keypoints_file=kps_file,
+            **cfg.dataloader
+        )
+    else:
+        raise ValueError(f"Unsupported dataset type: {dataset_type}")
 
+    #######################################################################################
     # Initialize model
     model = ModelClassDict[model_name](
         save_dir=save_dir,
@@ -74,6 +105,7 @@ def main(cfg: DictConfig):
         **cfg.model
     )
 
+    #######################################################################################
     # Initialize trainer
     trainer = L.Trainer(
         logger=wandb_logger,
@@ -81,25 +113,29 @@ def main(cfg: DictConfig):
     )
 
     # Train model
-    trainer.fit(model, datamodule=data_module, ckpt_path=cfg.get('checkpoint', None))
+    trainer.fit(model, datamodule=data_module, ckpt_path=ckpt_file)
 
-    # Copy the best model to the save directory
+    # Get the path and score of the best model
     best_model_path = trainer.checkpoint_callback.best_model_path
     best_model_score = trainer.checkpoint_callback.best_model_score
-    shutil.copyfile(best_model_path, os.path.join(os.path.dirname(best_model_path), 'best.ckpt'))
     print("Best Model:", best_model_path, ", Best DEV_WER:", best_model_score.item())
+
+    # Copy the best model to the save directory
+    shutil.copyfile(best_model_path, os.path.join(os.path.dirname(best_model_path), 'best.ckpt'))
 
     # Test the best model
     trainer.test(model, datamodule=data_module, ckpt_path=best_model_path)
 
+    #######################################################################################
     # Ensure wandb.finish() is called
     try:
         wandb.finish()
     except Exception as finish_error:
         print(f"wandb.finish() encountered an issue: {finish_error}")
 
+    #######################################################################################
     # Optionally convert model to ONNX format
-    if cfg.get('convert_to_onnx', False) and trainer.is_global_zero:
+    if is_save_onnx and trainer.is_global_zero:
         onnx_save_dir = os.path.join(save_dir, 'onnx')
         os.makedirs(onnx_save_dir, exist_ok=True)
         best_model = ModelClassDict[model_name].load_from_checkpoint(best_model_path)
