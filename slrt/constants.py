fa725b00ef73449d32c4c96e61d348024216732e
zjx <1240348479@qq.com>
2024-12-19 17:43:17 +0800

    modify something in configs

--- slrt/constants.py
+++ slrt/constants.py
@@ -7,7 +7,7 @@ from slrt.datasets.transforms import ToTensor, TemporalRescale, RandomDrop
 from slrt.datasets.transforms.keypoints import DefinedDorp, RandomMove
 
 CONFIG_PATH = '../configs'
-CONFIG_NAME = 'exp_mska_csldaily.yaml'
+CONFIG_NAME = 'config.yaml'
 
 DataModuleClassDict = {
     "phoenix2014": slrt.datasets.DataModules.Phoenix2014DataModule,
