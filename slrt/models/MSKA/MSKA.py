b39a6fd37b8313c7ea5a524258bb014b3f06a647
zjx <1240348479@qq.com>
2025-01-04 16:28:24 +0800

    update MSKA

--- slrt/models/MSKA/MSKA.py
+++ slrt/models/MSKA/MSKA.py
@@ -1,18 +1,14 @@
 from typing import Any
 
 import torch
-from torch import nn
+import wandb
 from typing_extensions import override
 
 from slrt.models.BaseModel.SLRTBaseModel import SLRTBaseModel
-from slrt.models.MSKA.modules.BiLSTM import BiLSTMLayer
 from slrt.models.MSKA.modules.DSTA import STAttentionModule
-from slrt.models.MSKA.modules.LstmHead import LstmHead
 from slrt.models.MSKA.modules.Visualhead import VisualHead
 # from slrt.models.MSKA.modules.translation import TranslationNetwork
 from slrt.models.MSKA.modules.vl_mapper import VLMapper
-
-import torch.nn.functional as F
 
 
 class MSKA(SLRTBaseModel):
@@ -25,65 +21,54 @@ class MSKA(SLRTBaseModel):
         self.save_hyperparameters()
         self.name = 'MSKA'
 
+        self.flag = True
+
     @override
     def _init_network(self, **kwargs):
+        ph = 7
+        pw = 7
         self.face_visual_backbone = STAttentionModule(
-            num_channel=3,
+            num_channel=pw * ph * 3 + 3,
             max_frame=400,
             num_node=len(self.hparams.kps_idx["face"]),
             st_attention_module_prams=self.hparams.network.st_attention_module_prams,
         )
         self.body_visual_backbone = STAttentionModule(
-            num_channel=3,
+            num_channel=pw * ph * 3 + 3,
             max_frame=400,
             num_node=len(self.hparams.kps_idx["body"]),
             st_attention_module_prams=self.hparams.network.st_attention_module_prams,
         )
         self.left_visual_backbone = STAttentionModule(
-            num_channel=3,
+            num_channel=pw * ph * 3 + 3,
             max_frame=400,
             num_node=len(self.hparams.kps_idx["left"]),
             st_attention_module_prams=self.hparams.network.st_attention_module_prams,
         )
         self.right_visual_backbone = STAttentionModule(
-            num_channel=3,
+            num_channel=pw * ph * 3 + 3,
             max_frame=400,
             num_node=len(self.hparams.kps_idx["right"]),
             st_attention_module_prams=self.hparams.network.st_attention_module_prams,
         )
 
-        # self.fuse_bilstm_layer = BiLSTMLayer(**self.hparams.network["fuse_BiLSTM"])
-        # self.body_bilstm_layer = BiLSTMLayer(**self.hparams.network["body_BiLSTM"])
-        # self.left_bilstm_layer = BiLSTMLayer(**self.hparams.network["left_BiLSTM"])
-        # self.right_bilstm_layer = BiLSTMLayer(**self.hparams.network["right_BiLSTM"])
-        #
-        # num_classes = len(self.recognition_tokenizer)
-        # # self.fuse_visual_head = VisualHead(
-        # #     cls_num=num_classes,
-        # #     **self.hparams.network.head_cfg['fuse_visual_head']
-        # # )
-        # # self.body_visual_head = VisualHead(
-        # #     cls_num=num_classes,
-        # #     **self.hparams.network.head_cfg['body_visual_head']
-        # # )
-        # # self.left_visual_head = VisualHead(
-        # #     cls_num=num_classes,
-        # #     **self.hparams.network.head_cfg['left_visual_head']
-        # # )
-        # # self.right_visual_head = VisualHead(
-        # #     cls_num=num_classes,
-        # #     **self.hparams.network.head_cfg['right_visual_head']
-        # # )
-        # self.fuse_linear = NormLinear(1024, num_classes)
-        # self.body_linear = NormLinear(256, num_classes)
-        # self.left_linear = NormLinear(512, num_classes)
-        # self.right_linear = NormLinear(512, num_classes)
-
         num_classes = len(self.recognition_tokenizer)
-        self.fuse_lstm_head = LstmHead(input_size=1024, hidden_size=512, num_classes=num_classes)
-        self.body_lstm_head = LstmHead(input_size=256, hidden_size=512, num_classes=num_classes)
-        self.left_lstm_head = LstmHead(input_size=512, hidden_size=512, num_classes=num_classes)
-        self.right_lstm_head = LstmHead(input_size=512, hidden_size=512, num_classes=num_classes)
+        self.fuse_visual_head = VisualHead(
+            cls_num=num_classes,
+            **self.hparams.network.head_cfg['fuse_visual_head']
+        )
+        self.body_visual_head = VisualHead(
+            cls_num=num_classes,
+            **self.hparams.network.head_cfg['body_visual_head']
+        )
+        self.left_visual_head = VisualHead(
+            cls_num=num_classes,
+            **self.hparams.network.head_cfg['left_visual_head']
+        )
+        self.right_visual_head = VisualHead(
+            cls_num=num_classes,
+            **self.hparams.network.head_cfg['right_visual_head']
+        )
 
         # if "translation" in self.task:
         #     self.translation_network = TranslationNetwork(cfg=self.hparams['TranslationNetwork'])
@@ -116,11 +101,24 @@ class MSKA(SLRTBaseModel):
             reduction="batchmean"
         )
 
-    def forward(self, kps, kps_lgt) -> Any:
+    def forward(self, patches, kps, patch_lgt, kps_lgt) -> Any:
+        for i in range(len(patches)):
+            assert patch_lgt[i] == kps_lgt[i]
+        if self.flag:
+            for i in range(patches.shape[2]):
+                img = patches[0, 0, i, :, :, :].permute(1, 2, 0).cpu().numpy()
+                self.logger.experiment.log({f"patch": wandb.Image(img)})
+            self.flag = False
+
         # 获取输入数据的维度
-        N, C, T, V = kps.shape
+        N, T, V, C = kps.shape
+        patches = patches.view(N, T, V, -1)
+        kps = torch.cat([kps, patches], dim=-1)
+
+        N, T, V, C = kps.shape
+
         # 调整输入数据的维度顺序并保持连续性
-        kps = kps.permute(0, 1, 2, 3).contiguous().view(N, C, T, V)
+        kps = kps.permute(0, 3, 1, 2).contiguous().view(N, C, T, V)
 
         # backbone forward
         face_backbone_output = self.face_visual_backbone(kps[:, :, :, self.hparams.kps_idx["face"]])
@@ -144,40 +142,27 @@ class MSKA(SLRTBaseModel):
             mask[i, :, :mask_lgt[i]] = 1
         mask = mask.to(torch.bool)
 
-        fuse_cat = fuse_cat.permute(1, 0, 2).contiguous()
-        body_cat = body_cat.permute(1, 0, 2).contiguous()
-        left_cat = left_cat.permute(1, 0, 2).contiguous()
-        right_cat = right_cat.permute(1, 0, 2).contiguous()
-        fuse_cat = self.fuse_lstm_head(fuse_cat, mask_lgt.cpu())
-        body_cat = self.body_lstm_head(body_cat, mask_lgt.cpu())
-        left_cat = self.left_lstm_head(left_cat, mask_lgt.cpu())
-        right_cat = self.right_lstm_head(right_cat, mask_lgt.cpu())
-        fuse_cat = fuse_cat.permute(1, 0, 2).contiguous()
-        body_cat = body_cat.permute(1, 0, 2).contiguous()
-        left_cat = left_cat.permute(1, 0, 2).contiguous()
-        right_cat = right_cat.permute(1, 0, 2).contiguous()
-
         # head forward
-        # fuse_head_output = self.fuse_visual_head(
-        #     x=fuse_cat,
-        #     mask=mask.to(self.device),
-        #     valid_len_in=mask_lgt.to(self.device)
-        # )
-        # left_head_output = self.left_visual_head(
-        #     x=left_cat,
-        #     mask=mask.to(self.device),
-        #     valid_len_in=mask_lgt.to(self.device)
-        # )
-        # right_head_output = self.right_visual_head(
-        #     x=right_cat,
-        #     mask=mask.to(self.device),
-        #     valid_len_in=mask_lgt.to(self.device)
-        # )
-        # body_head_output = self.body_visual_head(
-        #     x=body_cat,
-        #     mask=mask.to(self.device),
-        #     valid_len_in=mask_lgt.to(self.device)
-        # )
+        fuse_head_output = self.fuse_visual_head(
+            x=fuse_cat,
+            mask=mask.to(self.device),
+            valid_len_in=mask_lgt.to(self.device)
+        )
+        left_head_output = self.left_visual_head(
+            x=left_cat,
+            mask=mask.to(self.device),
+            valid_len_in=mask_lgt.to(self.device)
+        )
+        right_head_output = self.right_visual_head(
+            x=right_cat,
+            mask=mask.to(self.device),
+            valid_len_in=mask_lgt.to(self.device)
+        )
+        body_head_output = self.body_visual_head(
+            x=body_cat,
+            mask=mask.to(self.device),
+            valid_len_in=mask_lgt.to(self.device)
+        )
 
         # if "translation" in self.task:
         #     mapped_feature = self.vl_mapper(visual_outputs={"gloss_feature": fuse_head_output["gloss_probabilities"]})
@@ -194,10 +179,6 @@ class MSKA(SLRTBaseModel):
         #         'gloss_logits': fuse_head_output,
         #         'input_lengths': mask_lgt
         #     }
-        fuse_head_output = fuse_cat
-        left_head_output = left_cat
-        right_head_output = right_cat
-        body_head_output = body_cat
 
         return {
             'fuse_gloss_logits': fuse_head_output,
@@ -230,10 +211,10 @@ class MSKA(SLRTBaseModel):
         # y_translation_lgt = batch['translation_length']
         # name = batch['name']
 
-        x, y_glosses, y_translation, x_lgt, y_glosses_lgt, y_translation_lgt, name = batch
+        patches, kps, y_glosses, y_translation, patches_lgt, kps_lgt, y_glosses_lgt, y_translation_lgt, name = batch
         batch_size = len(name)
 
-        outputs = self.forward(x, x_lgt)
+        outputs = self.forward(patches, kps, patches_lgt, kps_lgt)
 
         if self.trainer.predicting:
             return torch.tensor([]), outputs['ensemble_last_gloss_logits'].permute(1, 0, 2), None, outputs[
@@ -296,63 +277,3 @@ class MSKA(SLRTBaseModel):
             "optimizer": optimizer,
             "lr_scheduler": scheduler
         }
-
-
-class NormLinear(nn.Module):
-    """
-    实现规范化线性层，继承自nn.Module。
-
-    该层的主要功能是对输入进行线性变换，其中线性变换的权重被规范化。
-    支持添加偏置项，但默认不添加。
-
-    Attributes:
-        weight (nn.Parameter): 线性变换的权重参数。
-        bias (nn.Parameter, optional): 偏置项参数，仅在add_bias为True时存在。
-    """
-
-    def __init__(self, in_dim, out_dim, add_bias=False):
-        """
-        初始化NormLinear层。
-
-        Parameters:
-            in_dim (int): 输入特征维度。
-            out_dim (int): 输出特征维度。
-            add_bias (bool, optional): 是否添加偏置项，默认为False。
-        """
-        super(NormLinear, self).__init__()
-        self.weight = nn.Parameter(torch.Tensor(in_dim, out_dim))
-        nn.init.xavier_uniform_(self.weight, gain=nn.init.calculate_gain('relu'))
-
-        self.add_bias = add_bias
-        if self.add_bias:
-            self.bias = nn.Parameter(torch.Tensor(out_dim))  # 添加偏置项
-            nn.init.zeros_(self.bias)  # 初始化偏置项为零
-
-    def forward(self, x):
-        """
-        对输入张量与规范化后的权重进行矩阵乘法操作，并加上偏置项。
-
-        Parameters:
-            x (torch.Tensor): 输入张量，形状为 (batch_size, sequence_length, feature_dim)。
-
-        Returns:
-            torch.Tensor: 输出张量，形状为 (batch_size, sequence_length, out_dim)。
-        """
-        # 验证输入张量的形状
-        if len(x.shape) != 3:
-            raise ValueError(f"Input tensor must be 3D, got shape {x.shape}")
-        batch_size, sequence_length, feature_dim = x.shape
-        if feature_dim != self.weight.shape[0]:
-            raise ValueError("Feature dimension of input does not match weight")
-
-        # 规范化权重
-        normalized_weight = F.normalize(self.weight, dim=0)
-
-        # 矩阵乘法
-        outputs = torch.matmul(x, normalized_weight)
-
-        if self.add_bias:
-            # 添加偏置项
-            outputs += self.bias  # 偏置项会自动广播以匹配输出的形状
-
-        return outputs
